78c5b0a0551bb626bdb6b501ae5ddbf05ea6af73
madistevens <madistevens@ocadu.ca>
2025-01-28 18:05:39 -0500

    First Version

---
+++ script.js
@@ -0,0 +1,14 @@
+var tree; // declared a variable for store space in memory;
+
+tree = 5; // definition with a data type of number with the value of 5;
+
+var Tree = 24; // semi are end of statement, this line has a declaration and definition in it;
+
+console.log(tree);
+
+tree = Tree; // = assignment operator, it assigns the value of the right side to the left side;
+
+console.log(tree);
+
+var cloud; // declared a variable but not defined a value;
+console.log(cloud); // undefined, because we have not assigned any value to it;
